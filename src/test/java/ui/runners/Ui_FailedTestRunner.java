e4b91322e90c5de9e1d118aed25602cdfb4126fa
Yusuf Sahin <69377854+flashh670@users.noreply.github.com>
2022-11-30 18:46:34 +0100

    Ui_FailedTestRuner have been updated by Yusuf Sahin.

--- src/test/java/ui/runners/Ui_FailedTestRunner.java
+++ src/test/java/ui/runners/Ui_FailedTestRunner.java
@@ -6,7 +6,7 @@ import org.junit.runner.RunWith;
 
 @RunWith(Cucumber.class)
 @CucumberOptions(
-        plugin = {"html:target/failed-html-report.html."},
+        plugin = {"html:target/failed-html-report.html"},
         features = "@target/rerun.txt",
         glue = "step_definitions"
 )
