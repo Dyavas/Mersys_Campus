7638378ab684f1d249101e11611ce26daf955428
Physiklehrer2018 <93185666+Physiklehrer2018@users.noreply.github.com>
2022-11-30 19:30:37 +0100

    Last updated

--- src/test/java/ui/pages/Dieburg.java
+++ src/test/java/ui/pages/Dieburg.java
@@ -2,6 +2,6 @@ package ui.pages;
 
 public class Dieburg {
     public static void main(String[] args) {
-        System.out.println("Hallo!!!");
+        System.out.println("Hallo Dieburg!!!");
     }
 }
