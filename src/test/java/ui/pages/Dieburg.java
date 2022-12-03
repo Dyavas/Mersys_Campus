f27e2f0d3a2cf1faa66ff810ef037ce5f5a8ed5f
Physiklehrer2018 <93185666+Physiklehrer2018@users.noreply.github.com>
2022-12-03 20:48:39 +0100

    New Dieburg info by Mehmet_Bozabali

--- src/test/java/ui/pages/Dieburg.java
+++ src/test/java/ui/pages/Dieburg.java
@@ -2,6 +2,6 @@ package ui.pages;
 
 public class Dieburg {
     public static void main(String[] args) {
-        System.out.println("Hallo Guten Abend Dieburg!!!");
+        System.out.println("Hallo Guten Abend ?????Dieburg!!!");
     }
 }
